aabde5f784bbee80aab8548a63fd4471a9792e72
Richard Joffray <rjoffray@richards-mbp.lan>
2019-04-12 12:05:47 -0700

    made buttons clearer disabled current sound card button added audio test file fixes #1, fixes #2, fixes #3

--- src/platform.js
+++ src/platform.js
@@ -1,20 +1,15 @@
 
 const { join } = require('path')
 const OakPlatform = require('@oaklabs/platform')
-const oak = require('oak')
-const _ = require('lodash')
+const YAML = require('yamljs')
 
-const YAML = require('yamljs');
-
-var image = ''
 let window = null
 
-function initPlatform(win) {
+function initPlatform (win) {
   window = win
 }
 async function getAudioInfo (cb = function () {}) {
-  // open a connection to the platform host
-  console.log("PLATFORM_HOST", process.env.PLATFORM_HOST )
+  console.log('PLATFORM_HOST', process.env.PLATFORM_HOST)
   let platform = await new OakPlatform({
     host: process.env.PLATFORM_HOST || 'localhost:443'
   })
@@ -24,8 +19,7 @@ async function getAudioInfo (cb = function () {}) {
 }
 
 async function configureAudio (config, cb = function () {}) {
-  // open a connection to the platform host
-  console.log("PLATFORM_HOST", process.env.PLATFORM_HOST )
+  console.log('PLATFORM_HOST', process.env.PLATFORM_HOST)
   let platform = await new OakPlatform({
     host: process.env.PLATFORM_HOST || 'localhost:443'
   })
@@ -35,15 +29,15 @@ async function configureAudio (config, cb = function () {}) {
 
 async function reinstallApplication (card) {
   let platform = await new OakPlatform({
-      host: process.env.PLATFORM_HOST || 'localhost:443'
+    host: process.env.PLATFORM_HOST || 'localhost:443'
   })
-
   let app = await platform.application()
 
-  async function getLiveImageName() {
-    return new Promise(async function(resolve, reject) {
+  async function getLiveImageName () {
+    return new Promise(async function (resolve, reject) {
       try {
         await app.viewLive(undefined, function (err, res) {
+          if (err) throw err
           let services = YAML.parse(res.docker_compose_yaml).services
           let image = services[Object.keys(services)[0]].image
           console.log('- LIVE:', image)
@@ -61,43 +55,40 @@ async function reinstallApplication (card) {
         image: await getLiveImageName(),
         environment: {
           ALSA_CARD: card,
-          NODE_ENV: "production"
+          NODE_ENV: 'production'
         }
       }
     ]
   }, function (err) {
-      console.log('- ERROR', err)
+    console.log('- ERROR', err)
   })
-  .on('data', function (data) {
-    console.log(data.step)
-    window.send('installProgress', data)
-  })
-  .on('end', async function () {
-    let message = {
-      details: '',
-      step: 'DONE INSTALLING'
-    }
-    window.send('installProgress', message)
-    await app.swapIdleAndLive(undefined, async function () {
+    .on('data', function (data) {
+      console.log(data.step)
+      window.send('installProgress', data)
+    })
+    .on('end', async function () {
       let message = {
         details: '',
-        step: 'SWAPPED IDLE AND LIVE'
+        step: 'DONE INSTALLING'
       }
       window.send('installProgress', message)
-      await app.viewLive(undefined, function (err, res) {
-        let services = YAML.parse(res.docker_compose_yaml).services
-        let image = services[Object.keys(services)[0]].image
-        console.log('- LIVE:', image)
+      await app.swapIdleAndLive(undefined, async function () {
+        let message = {
+          details: '',
+          step: 'SWAPPED IDLE AND LIVE'
+        }
+        window.send('installProgress', message)
+        await app.viewLive(undefined, function (err, res) {
+          if (err) throw err
+          let services = YAML.parse(res.docker_compose_yaml).services
+          let image = services[Object.keys(services)[0]].image
+          console.log('- LIVE:', image)
+        })
       })
     })
-  })
 }
-  
 
 module.exports.reinstallApplication = reinstallApplication
 module.exports.getAudioInfo = getAudioInfo
 module.exports.configureAudio = configureAudio
 module.exports.initPlatform = initPlatform
-
-
-
